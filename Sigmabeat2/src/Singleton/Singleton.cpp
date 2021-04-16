5bd61124de48813bbf1318d2499396872612a260
Ryoga-exe <wwwryoga1216www@gmail.com>
2021-04-16 18:31:36 +0900

    Add: Singleton

---
+++ Sigmabeat2/src/Singleton/Singleton.cpp
@@ -0,0 +1,22 @@
+﻿#include "Singleton.hpp"
+
+namespace {
+    constexpr int kMaxFinalizersSize = 256;
+    std::mutex gMutex;
+    int gNumFinalizersSize = 0;
+    SingletonFinalizer::FinalizerFunc gFinalizers[kMaxFinalizersSize];
+}
+
+void SingletonFinalizer::addFinalizer(FinalizerFunc func) {
+    std::lock_guard<std::mutex> lock(gMutex);
+    assert(gNumFinalizersSize < kMaxFinalizersSize);
+    gFinalizers[gNumFinalizersSize++] = func;
+}
+
+void SingletonFinalizer::finalize() {
+    std::lock_guard<std::mutex> lock(gMutex);
+    for (int i = gNumFinalizersSize - 1; i >= 0; i--) {
+        (*gFinalizers[i])();
+    }
+    gNumFinalizersSize = 0;
+}
---
+++ Sigmabeat2/src/Singleton/Singleton.hpp
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include <cassert>
+#include <mutex>
+
+class SingletonFinalizer {
+public:
+    using FinalizerFunc = void(*)();
+    static void addFinalizer(FinalizerFunc func);
+    static void finalize();
+};
+
+template <typename T>
+class Singleton final {
+public:
+    static T& get_instance() {
+        std::call_once(initFlag, create);
+        assert(instance);
+        return *instance;
+    }
+
+private:
+    static void create() {
+        instance = new T;
+        SingletonFinalizer::addFinalizer(&Singleton<T>::destroy);
+    }
+
+    static void destroy() {
+        delete instance;
+        instance = nullptr;
+    }
+
+    static std::once_flag initFlag;
+    static T* instance;
+};
+
+template <typename T> std::once_flag Singleton<T>::initFlag;
+template <typename T> T* Singleton<T>::instance = nullptr;
