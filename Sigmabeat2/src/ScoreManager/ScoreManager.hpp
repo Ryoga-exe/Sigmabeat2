0353064ed150259d38c689307cd486271e2155ad
Ryoga-exe <wwwryoga1216www@gmail.com>
2021-04-16 18:32:10 +0900

    Update: ScoreManager class

--- Sigmabeat2/src/ScoreManager/ScoreManager.hpp
+++ Sigmabeat2/src/ScoreManager/ScoreManager.hpp
@@ -1,3 +1,26 @@
 ﻿#pragma once
 #include "../Common.hpp"
 
+namespace Score {
+
+    struct Data {
+        String title;
+        String artist;
+        String category;
+        String url;
+        FilePath path;
+        FilePath musicPath;
+        FilePath imagePath;
+
+
+        Color backgroundColor[4];
+
+    };
+
+    class Manager {
+    public:
+
+    private:
+        Array<Data> m_scores;
+    };
+}
