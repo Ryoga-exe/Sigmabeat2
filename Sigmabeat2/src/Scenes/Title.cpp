fb4a1a3fc6ac84a541eddcc1754e5a943d7fd52d
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2021-04-17 20:49:16 +0900

    Update: Title Scene

--- Sigmabeat2/src/Scenes/Title.cpp
+++ Sigmabeat2/src/Scenes/Title.cpp
@@ -18,17 +18,17 @@ void Title::draw() const {
 
     const double t = m_delta / 75.0;
     auto [x, y] = Scene::Center();
-    m_titleLogo.scaled(Window::ClientWidth() / (2100.0 * EaseOutExpo(t))).drawAt(x, y /* - Window::ClientHeight() / 10 * EaseInOutExpo(Max(0.0, t - 1.2)) */ );
+    m_titleLogo.scaled(Min(Window::ClientHeight(), Window::ClientWidth()) / (1100.0 * EaseOutExpo(t))).drawAt(x, y /* - Window::ClientHeight() / 10 * EaseInOutExpo(Max(0.0, t - 1.2)) */ );
     
     {
-        const Transformer2D tf2(Mat3x2::Scale(Window::ClientHeight() / 1000.0, { x, y + y / 1.5 }));
+        const Transformer2D tf2(Mat3x2::Scale(Min(Window::ClientHeight(), Window::ClientWidth()) / 1000.0, { x, y + y / 1.5 }));
 
         FontAsset(U"Title")(U" - PRESS START KEY TO START - ")
             .drawAt(x, y + y / 1.5, Color(100, (uint32)(((Sin(m_delta / 45.0) + 1.0) / 2.0) * 255)));
     }
     {
         String version = U"1.0.0";
-        const Transformer2D tf2(Mat3x2::Scale(Window::ClientHeight() / 1250.0));
+        const Transformer2D tf2(Mat3x2::Scale(Min(Window::ClientHeight(), Window::ClientWidth()) / 1250.0));
 
         FontAsset(U"Title")(U" Version ", version).draw(Arg::topLeft = Vec2{ 0, 0 }, Color(50));
     }
