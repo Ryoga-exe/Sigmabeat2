e548783a388e76168b4aa89e6e6c8103f12d477a
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2021-05-14 21:42:47 +0900

    Update: Config file

--- Sigmabeat2/src/Config/Config.cpp
+++ Sigmabeat2/src/Config/Config.cpp
@@ -7,7 +7,13 @@ namespace Config {
 
         if (!toml) return false;
 
+        data.windowSize = { toml[U"Window.width"].get<int32>(), toml[U"Window.height"].get<int32>() };
+        data.windowSizable = toml[U"Window.sizable"].get<bool>();
 
+        for (const auto& object : toml[U"Score.directory"].arrayView())
+        {
+            data.scoreDirectory << object.get<String>();
+        }
 
         return true;
     }
--- Sigmabeat2/src/Config/Config.hpp
+++ Sigmabeat2/src/Config/Config.hpp
@@ -5,8 +5,8 @@ namespace Config {
     struct Data {
         Size windowSize = { 1280, 720 };
         bool windowSizable = true;
+        Array<String> scoreDirectory;
     };
 
     bool Load(Data& data);
-
 }
