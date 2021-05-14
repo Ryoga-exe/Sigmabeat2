6376d11770d04c3ec72f030f2d5af073c4e3297d
Ryoga-exe <wwwryoga1216www@gmail.com>
2021-05-14 21:07:55 +0900

    Add: Config

---
+++ Sigmabeat2/src/Config/Config.cpp
@@ -0,0 +1,14 @@
+﻿#include "../Common.hpp"
+#include "Config.hpp"
+
+namespace Config {
+    bool Load(Data& data) {
+        const TOMLReader toml(U"config.toml");
+
+        if (!toml) return false;
+
+
+
+        return true;
+    }
+}
---
+++ Sigmabeat2/src/Config/Config.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "../Common.hpp"
+
+namespace Config {
+    struct Data {
+        Size windowSize = { 1280, 720 };
+        bool windowSizable = true;
+    };
+
+    bool Load(Data& data);
+
+}
