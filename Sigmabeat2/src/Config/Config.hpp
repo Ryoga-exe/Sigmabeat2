af06474972c24128da534ec99bc62043bae6c257
Ryoga.exe <ryoga.dev@gmail.com>
2021-06-08 21:22:06 +0900

    [Config] Update: change little

--- Sigmabeat2/src/Config/Config.hpp
+++ Sigmabeat2/src/Config/Config.hpp
@@ -6,7 +6,6 @@ namespace Config {
         Size windowSize = { 1280, 720 };
         bool windowSizable = true;
         Array<FilePath> scoreDirectory;
-        // String version;
     };
 
     bool Load(Data& data);
