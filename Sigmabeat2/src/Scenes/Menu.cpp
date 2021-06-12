df01f279288c36146bc304165a54eeab8c7cb551
Ryoga.exe <ryoga.dev@gmail.com>
2021-06-13 00:23:01 +0900

    [Scene] Update: Menu

--- Sigmabeat2/src/Scenes/Menu.cpp
+++ Sigmabeat2/src/Scenes/Menu.cpp
@@ -116,12 +116,12 @@ void Menu::draw() const {
 
     if (m_indexSize == 0) return;
 
-    RectF(Arg::bottomCenter = Vec2{ Scene::CenterF().x, m_tileBaseY }, Scene::Width(), UI::Menu::NormalTileSize.y).stretched(50).draw(ColorF(0, 0.5));
+    RectF(Arg::bottomCenter = Vec2{ Scene::CenterF().x, m_tileBaseY - 30 }, Scene::Width(), UI::Menu::NormalTileSize.y).stretched(80).draw(ColorF(0, 0.5));
     
-    Quad(Vec2(Scene::CenterF().x - UI::Menu::TileSize.x / 2.0 - UI::Menu::TileMargin - UI::Menu::SelectedTileMarginSize, m_tileBaseY - UI::Menu::NormalTileSize.y - 50)
-        ,Vec2(Scene::CenterF().x + UI::Menu::TileSize.x / 2.0 + UI::Menu::TileMargin + UI::Menu::SelectedTileMarginSize, m_tileBaseY - UI::Menu::NormalTileSize.y - 50)
-        ,Vec2(Scene::CenterF().x + UI::Menu::TileSize.x / 2.0, m_tileBaseY - UI::Menu::NormalTileSize.y - 200)
-        ,Vec2(Scene::CenterF().x - UI::Menu::TileSize.x / 2.0, m_tileBaseY - UI::Menu::NormalTileSize.y - 200)).draw(ColorF(0, 0.5));
+    Quad(Vec2(Scene::CenterF().x - UI::Menu::TileSize.x / 2.0 - UI::Menu::TileMargin - UI::Menu::SelectedTileMarginSize, m_tileBaseY - UI::Menu::NormalTileSize.y - 80 - 30)
+        ,Vec2(Scene::CenterF().x + UI::Menu::TileSize.x / 2.0 + UI::Menu::TileMargin + UI::Menu::SelectedTileMarginSize, m_tileBaseY - UI::Menu::NormalTileSize.y - 80 - 30)
+        ,Vec2(Scene::CenterF().x + UI::Menu::TileSize.x / 2.0, m_tileBaseY - UI::Menu::NormalTileSize.y - 200 - 30)
+        ,Vec2(Scene::CenterF().x - UI::Menu::TileSize.x / 2.0, m_tileBaseY - UI::Menu::NormalTileSize.y - 200 - 30)).draw(ColorF(0, 0.5));
 
     drawTiles();
 
