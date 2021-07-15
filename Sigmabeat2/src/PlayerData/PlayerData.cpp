e9ac0d6f8d9a7461ab56fb4cc5776f1ec51084ca
Ryoga.exe <ryoga.dev@gmail.com>
2021-07-16 03:31:47 +0900

    Add PlayerData

---
+++ Sigmabeat2/src/PlayerData/PlayerData.cpp
@@ -0,0 +1,2 @@
+﻿#include "PlayerData.hpp"
+
---
+++ Sigmabeat2/src/PlayerData/PlayerData.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "Common.hpp"
+
+namespace PlayerData {
+
+	struct Data {
+		String name;
+		int32  level;
+		int32  rating;
+	};
+
+}
