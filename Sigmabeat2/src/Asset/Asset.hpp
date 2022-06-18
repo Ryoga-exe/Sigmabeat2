ea280bb3c35b3b0e4cd9a2706a405c9efebb561a
Ryoga.exe <ryoga.dev@gmail.com>
2022-06-19 02:09:12 +0900

    [Assets] chore

--- Sigmabeat2/src/Asset/Asset.hpp
+++ Sigmabeat2/src/Asset/Asset.hpp
@@ -2,14 +2,14 @@
 #include "Common.hpp"
 
 namespace Asset {
-    constexpr StringView OpeningWav = U"assets/sounds/BGM/opening.wav";
-    constexpr StringView ResultWav = U"assets/sounds/BGM/result.wav";
-    constexpr StringView Select_SE = U"assets/sounds/SE/Select_SE.wav";
-    constexpr StringView LevelChange_SE = U"assets/sounds/SE/LevelChange_SE.ogg";
+    constexpr FilePathView OpeningWav = U"assets/sounds/BGM/opening.wav";
+    constexpr FilePathView ResultWav = U"assets/sounds/BGM/result.wav";
+    constexpr FilePathView Select_SE = U"assets/sounds/SE/Select_SE.wav";
+    constexpr FilePathView LevelChange_SE = U"assets/sounds/SE/LevelChange_SE.ogg";
 
     namespace Image {
-        constexpr StringView Logo = U"assets/images/logos/sigmabeat.png";
-        constexpr StringView Ryoga_exe = U"assets/images/logos/ryoga-exe.png";
+        constexpr FilePathView Logo = U"assets/images/logos/sigmabeat.png";
+        constexpr FilePathView Ryoga_exe = U"assets/images/logos/ryoga-exe.png";
     }
 
     void Init();
